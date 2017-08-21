4da186a04fbbb2a58ef6de46050081c7deac1a6e
abe <abe>
2017-08-22 01:26:06 +0900

    fdfd

--- Test1.py
+++ Test1.py
@@ -26,8 +26,14 @@ def greet():
 greet()
 '''
 
-holiday_str_list = ['2017/8/22', '2017/8/25', '2017/7/21']
+holiday_str_list = ['2017/8/20', '2017/8/21', '2017/8/25', '2017/7/21']
 holiday_date_list = [datetime.datetime.strptime(x, "%Y/%m/%d").date() for x in holiday_str_list]
+
+def check_output_date_list(today_date, check_date_list):
+    yeasterday = today_date - datetime.timedelta(days=1)
+    check_date_list.append(yeasterday)
+    if yeasterday in holiday_date_list:
+        check_output_date_list(yeasterday, check_date_list)
 
 print (holiday_date_list)
 
@@ -35,10 +41,16 @@ today= datetime.datetime.today()
 yeasterday = today.date() - datetime.timedelta(days=1)
 
 print (today) 
-print (yeasterday)
+#print (yeasterday)
 
-if  yeasterday in holiday_date_list:
-    print('hit')
+output_date_list = []
+check_output_date_list(today.date(), output_date_list)
+
+print output_date_list
+
+
+    
+
 
 
 
