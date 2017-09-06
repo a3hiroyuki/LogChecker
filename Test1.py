6e2f0580d1a01e833d8c0dddd0ec98c4fb54dd19
abe <abe>
2017-09-07 02:54:29 +0900

    fdf

--- Test1.py
+++ Test1.py
@@ -1,52 +1,58 @@
 '''
-Created on 2016/07/14
+Created on 2016/09/07
 
-@author: hiroy
+@author: Abe
 '''
 import datetime
-'''
-weekMap = {('Mon', 1):1, ('Thr',2):2, ('Wed',3):3}
+import os
+import pandas as pd
 
-if ('Mon', 1) in weekMap:
-    print 'True'
-    
-weekMap.get(('Mon, 1'), 'nai')
-
-print weekMap.values()
-print weekMap.keys()
-
-del(weekMap[('Mon', 1)])
-
-for k, c in weekMap.items():
-    print str(k) + str(c)
-    
-def greet():
-    print 'hello'
-    
-greet()
-'''
-
-holiday_str_list = ['2017/8/20', '2017/8/21', '2017/8/25', '2017/7/21']
+holiday_str_list = ['2017/9/1','2017/9/2', '2017/9/3']
 holiday_date_list = [datetime.datetime.strptime(x, "%Y/%m/%d").date() for x in holiday_str_list]
 
-def check_output_date_list(today_date, check_date_list):
-    yeasterday = today_date - datetime.timedelta(days=1)
-    check_date_list.append(yeasterday)
-    if yeasterday in holiday_date_list:
-        check_output_date_list(yeasterday, check_date_list)
+def check_new_data(df, yesterday):
+    temp_df = df[(df['aaa'] == '○') & (df['Date'] == yesterday)]['ID']
+    id_list = list(temp_df)
+    if len(id_list) > 0:
+        df2 = df[(df['ID'].isin(id_list)) & (df['Date'] < yesterday)]
+        print (df2)
+        df3 = df2.groupby(['Judge', 'ID']).count()
+        df4 = df3[df3['Date'] == 1]
+        id_list = list(df4.index)
+        if len(id_list) > 0:
+            id_list = [x[1] for x in id_list]
+            df.loc[(df['ID'].isin(id_list)) & (df['aaa'] == '○'), 'bbb'] = '○'
+            df = df[~(df['ID'].isin(id_list)) & ~(df['Date'] < yesterday)]
+            print(df)
+    return df
 
-print (holiday_date_list)
+def check_output_date_list(today_date, check_date_list, df):
+    yesterday = today_date - datetime.timedelta(days=1)
+    #errorの場合フラグを立てる
+    df.loc[(df['Date'] == yesterday) & (df['Judge'] == '×'), 'aaa'] = '○'
+    df = check_new_data(df, yesterday)
+    check_date_list.append(yesterday)
+    if yesterday in holiday_date_list:
+        check_output_date_list(yesterday, check_date_list, df)
 
-today= datetime.datetime.today()
-yeasterday = today.date() - datetime.timedelta(days=1)
+#print (holiday_date_list)
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+df = pd.read_csv(parent_dir+ "\\aaa.csv")
+df['Date'] = df['Date'].apply(lambda x:datetime.datetime.strptime(x,"%Y/%m/%d"))
+df['aaa'] = str('')
+df['bbb'] = str('')
+print (df)
+
+today = datetime.datetime.strptime('2017/9/4', "%Y/%m/%d")
+#today= datetime.datetime.today()
+yesterday = today.date() - datetime.timedelta(days=1)
 
 print (today) 
-#print (yeasterday)
 
 output_date_list = []
-check_output_date_list(today.date(), output_date_list)
+check_output_date_list(today.date(), output_date_list, df)
 
-print output_date_list
+print (output_date_list)
 
 
     
